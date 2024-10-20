339a354cb52854fd7eafa1c1dba6dda84a9fd41b
Ajay <g.ajaykanth666@gmail.com>
2024-10-20 10:22:31 +0530

    updated frequency graph

--- Sign.py
+++ Sign.py
@@ -12,3 +12,27 @@ n = np.arange(N)
 T = N/fs
 freq = n/T
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generate a sample sine wave
+fs = 1000  # Sampling frequency
+t = np.arange(0, 1, 1/fs)  # Time vector
+freq = 5  # Frequency of the sine wave
+x = np.sin(2 * np.pi * freq * t)
+
+# Compute the Fourier Transform
+X = np.fft.fft(x)
+N = len(X)
+n = np.arange(N)
+T = N/fs
+freq = n/T
+
+# Plot the frequency spectrum
+plt.figure(figsize=(12, 6))
+plt.plot(freq[:N//2], np.abs(X)[:N//2])
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
+plt.title('Frequency Spectrum of a Sample Sine Wave')
+plt.grid()
+plt.show()
