6e6892586451f5b1b49e7d4ae28f9bd11db3e94b
Ajay <g.ajaykanth666@gmail.com>
2024-10-20 10:26:19 +0530

    updated graph info

--- Sign.py
+++ Sign.py
@@ -5,7 +5,7 @@ t = np.arange(0, 1, 1/fs)              # Time vector
 freq = 5                               # Frequency of the sine wave
 x = np.sin(2 * np.pi * freq * t)
 
-                                       # Compute the Fourier Transform
+                                       # Compute the Fourier Transforms
 X = np.fft.fft(x)
 N = len(X)
 n = np.arange(N)
